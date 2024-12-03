b6a0589128b055bd324448d9f685a7a8f03ea633
Skittlez <Skittlez1397@gmail.com>
2024-12-02 21:46:21 -0600

    Update Day 2 using zipWithNext { }

--- src/aoc_2024/aoc2_24/aoc2_24.kt
+++ src/aoc_2024/aoc2_24/aoc2_24.kt
@@ -10,7 +10,7 @@ fun aoc2P1_24(input: List<String>) {
 
     input.forEach { line ->
         val digits = oneOrMoreDigits.findAll(line).map { it.value.toInt() }.toList()
-        if (safeLine(digits) < 0) {
+        if (safeLine(digits)) {
             total++
         }
     }
@@ -22,13 +22,13 @@ fun aoc2P2_24(input: List<String>) {
 
     input.forEach { line ->
         val digits = oneOrMoreDigits.findAll(line).map { it.value.toInt() }.toList()
-        if (safeLine(digits) < 0) {
+        if (safeLine(digits)) {
             total++
             return@forEach
         }
         for (i in digits.indices) {
             val newDigits = digits.filterIndexed { index, _ -> index != i  }
-            if (safeLine(newDigits) < 0) {
+            if (safeLine(newDigits)) {
                 total++
                 return@forEach
             }
@@ -39,29 +39,10 @@ fun aoc2P2_24(input: List<String>) {
     println("Total: $total")
 }
 
-private fun safeIncrement(last: Int, current: Int, positive: Boolean?): Boolean {
-    val diff = last - current
-    if (abs(diff) in 1..3) {
-        positive?.let {
-            if (positive != diff > 0) {
-                return false
-            }
-        }
-    } else {
-        return false
-    }
-    return true
-}
-
 // returns problematic index
-private fun safeLine(digits: List<Int>): Int {
-    var positive: Boolean? = null
-
-    for (i in 1..<digits.size) {
-        if (!safeIncrement(digits[i-1], digits[i], positive)) {
-            return i
-        }
-        if (i == 1) positive = digits[0] - digits[1] > 0
-    }
-    return -1
+private fun safeLine(digits: List<Int>): Boolean {
+    val diffs = digits.zipWithNext { a, b -> b - a }
+    val increasing = diffs.all { it in 1..3 }
+    val decreasing = diffs.all { it in -3..-1 }
+    return increasing || decreasing
 }
