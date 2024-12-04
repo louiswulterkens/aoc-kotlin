800d76d406e8c44e191cb196c39e927fb5b843bd
Skittlez <Skittlez1397@gmail.com>
2024-12-04 13:10:28 -0600

    Day 4, bruh moment on the diagonal vs cardinal X's, random reddit post baited me.

--- src/Main.kt
+++ src/Main.kt
@@ -6,6 +6,8 @@ import aoc_2024.aoc2_24.aoc2P1_24
 import aoc_2024.aoc2_24.aoc2P2_24
 import aoc_2024.aoc3_24.aoc3P1_24
 import aoc_2024.aoc3_24.aoc3P2_24
+import aoc_2024.aoc4_24.aoc4P1_24
+import aoc_2024.aoc4_24.aoc4P2_24
 import java.io.File
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -28,8 +30,11 @@ fun aoc2024() {
 //    aoc2P1_24(getFileAsString(fileName(problem = 2)))
 //    aoc2P2_24(getFileAsString(fileName(problem = 2)))
 
-    aoc3P1_24(getFileAsString(fileName(problem = 3)))
-    aoc3P2_24(getFileAsString(fileName(problem = 3)))
+//    aoc3P1_24(getFileAsString(fileName(problem = 3)))
+//    aoc3P2_24(getFileAsString(fileName(problem = 3)))
+
+//    aoc4P1_24(getFileAsString(fileName(problem = 4)))
+    aoc4P2_24(getFileAsString(fileName(problem = 4)))
 
 }
 
